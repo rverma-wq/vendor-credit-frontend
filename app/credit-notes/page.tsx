bc594e97a0bf90591e8980fdb0451f0aeeb92406
Rakshit Verma <r.verma@ubiquedigital.com>
2026-06-03 14:30:33 +0530

    Initial Next.js frontend setup

---
+++ app/credit-notes/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function CreditNotesPage() {
+  const [creditNotes, setCreditNotes] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchCreditNotes();
+  }, []);
+
+  const fetchCreditNotes = async () => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/credit-notes"
+      );
+
+      setCreditNotes(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const totalPurchase = creditNotes.reduce(
+    (sum, note) => sum + note.purchase_amount,
+    0
+  );
+  
+  const totalPaid = creditNotes.reduce(
+    (sum, note) => sum + note.paid_amount,
+    0
+  );
+  
+  const totalPending = creditNotes.reduce(
+    (sum, note) => sum + note.pending_amount,
+    0
+  );
+
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold mb-6">
+        Credit Notes
+      </h1>
+  
+      <table className="w-full border-collapse border">
+  <thead>
+    <tr>
+      <th className="border p-3 text-left">
+        Vendor
+      </th>
+      <th className="border p-3 text-left">
+        Purchase
+      </th>
+      <th className="border p-3 text-left">
+        Paid
+      </th>
+      <th className="border p-3 text-left">
+        Pending
+      </th>
+    </tr>
+  </thead>
+
+  <tbody>
+    {creditNotes.map((note) => (
+      <tr key={note.id}>
+        <td className="border p-3">
+          {note.vendor_name}
+        </td>
+
+        <td className="border p-3">
+          ₹{note.purchase_amount}
+        </td>
+
+        <td className="border p-3">
+          ₹{note.paid_amount}
+        </td>
+
+        <td className="border p-3">
+          ₹{note.pending_amount}
+        </td>
+      </tr>
+    ))}
+  </tbody>
+  <tfoot>
+  <tr className="font-bold ">
+    <td className="border p-3">
+      Total
+    </td>
+
+    <td className="border p-3">
+      ₹{totalPurchase.toLocaleString()}
+    </td>
+
+    <td className="border p-3">
+      ₹{totalPaid.toLocaleString()}
+    </td>
+
+    <td className="border p-3">
+      ₹{totalPending.toLocaleString()}
+    </td>
+  </tr>
+</tfoot>
+</table>
+    </main>
+  );
+}
---
+++ app/vendors/page.tsx
@@ -0,0 +1,218 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
+export default function VendorsPage() {
+    const [vendors, setVendors] = useState<any[]>([]);
+
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+    const [showModal, setShowModal] = useState(false);
+
+    const [selectedVendorId, setSelectedVendorId] =
+        useState("");
+
+    const [purchaseAmount, setPurchaseAmount] =
+        useState("");
+
+    const [paidAmount, setPaidAmount] =
+        useState("");
+
+    useEffect(() => {
+        fetchVendors();
+    }, []);
+
+    const createCreditNote = async () => {
+        try {
+          await axios.post(
+            "http://127.0.0.1:8000/credit-notes",
+            {
+              vendor_id: selectedVendorId,
+              purchase_amount: Number(purchaseAmount),
+              paid_amount: Number(paidAmount),
+            }
+          );
+      
+          alert("Credit Note Created");
+      
+          setShowModal(false);
+      
+          setPurchaseAmount("");
+          setPaidAmount("");
+      
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+    const fetchVendors = async () => {
+        try {
+            const response = await axios.get(
+                "http://127.0.0.1:8000/vendors"
+            );
+
+            setVendors(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const addVendor = async () => {
+        try {
+            await axios.post(
+                "http://127.0.0.1:8000/vendors",
+                {
+                    name,
+                    phone,
+                    address,
+                }
+            );
+
+            setName("");
+            setPhone("");
+            setAddress("");
+
+            fetchVendors();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const deleteVendor = async (vendorId: number) => {
+        try {
+            await axios.delete(
+                `http://127.0.0.1:8000/vendors/${vendorId}`
+            );
+
+            fetchVendors();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <main className="p-10">
+            <div className="border rounded p-6 mb-8">
+                <h2 className="font-bold text-xl mb-4">
+                    Add Vendor
+                </h2>
+
+                <div className="flex flex-col gap-3">
+
+                    <input
+                        className="border p-2 rounded"
+                        placeholder="Vendor Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+
+                    <input
+                        className="border p-2 rounded"
+                        placeholder="Phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+
+                    <input
+                        className="border p-2 rounded"
+                        placeholder="Address"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                    />
+
+                    <button
+                        onClick={addVendor}
+                        className="bg-black text-white p-2 rounded"
+                    >
+                        Add Vendor
+                    </button>
+
+                </div>
+            </div>
+            <h1 className="text-3xl font-bold mb-6">
+                Vendors
+            </h1>
+
+
+            <div className="space-y-4">
+                {vendors.map((vendor) => (
+                    <div
+                        key={vendor.id}
+                        className="border rounded p-4"
+                    >
+                        <h2 className="font-bold">
+                            {vendor.name}
+                        </h2>
+
+                        <p>{vendor.phone}</p>
+
+                        <p>{vendor.address}</p>
+                        <button
+                            onClick={() => deleteVendor(vendor.id)}
+                            className="mt-3 bg-red-700 text-white px-3 py-1 rounded"
+                        >
+                            Delete
+                        </button>
+                        <button
+                            onClick={() => {
+                                setSelectedVendorId(vendor.id);
+                                setShowModal(true);
+                            }}
+                            className="m-3 mt-3 bg-red-700 text-white px-3 py-1 rounded"
+                        >
+                            Add Credit Note
+                        </button>
+                    </div>
+                ))}
+            </div>
+            {showModal && (
+                <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
+                    <div className="bg-black p-6 rounded w-96">
+
+                        <h2 className="text-xl font-bold mb-4">
+                            Create Credit Note
+                        </h2>
+
+                        <input
+                            type="number"
+                            placeholder="Purchase Amount"
+                            value={purchaseAmount}
+                            onChange={(e) =>
+                                setPurchaseAmount(e.target.value)
+                            }
+                            className="border p-2 w-full mb-3"
+                        />
+
+                        <input
+                            type="number"
+                            placeholder="Paid Amount"
+                            value={paidAmount}
+                            onChange={(e) =>
+                                setPaidAmount(e.target.value)
+                            }
+                            className="border p-2 w-full mb-4"
+                        />
+
+                        <div className="flex gap-2">
+                            <button
+                                onClick={createCreditNote}
+                                className="bg-blue-500 text-white px-4 py-2 rounded"
+                            >
+                                Save
+                            </button>
+
+                            <button
+                                onClick={() => setShowModal(false)}
+                                className="bg-gray-500 text-white px-4 py-2 rounded"
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </main>
+    );
+}
